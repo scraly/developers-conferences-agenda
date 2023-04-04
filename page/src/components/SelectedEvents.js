8ead40896930b2d9472ff7c9516596b274b8fe31
Damien Vander Stricht <86291631+Damien-VS@users.noreply.github.com>
2023-04-04 15:03:05 +0200

    updated selected events date print format
    fixed the date format to double-digits

--- page/src/components/SelectedEvents.js
+++ page/src/components/SelectedEvents.js
@@ -5,7 +5,7 @@ import 'styles/SelectedEvents.css';
 const SelectedEvents = ({ events, date }) => {
 	return (
 	<>
-		<h3 className="eventDateDisplay">{date.getFullYear()}-{date.getMonth() + 1}-{date.getDate()}</h3>
+		<h3 className="eventDateDisplay">{(date.getFullYear()}-({('0' + date.getMonth() + 1).slice(-2)}-{('0' + date.getDate()).slice(-2)}</h3>
 		<div className="eventsGridDisplay">
 			{events?.map(e => <EventDisplay {...e} />)}
 		</div>
