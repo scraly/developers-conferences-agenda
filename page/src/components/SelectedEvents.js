fdd8c73acc524d275e341341a5003e470cea38e0
Damien Vander Stricht <damien.vanderstricht@ext.adeo.com>
2023-04-05 17:32:44 +0200

    extracted lpad for readbility

--- page/src/components/SelectedEvents.js
+++ page/src/components/SelectedEvents.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import 'styles/SelectedEvents.css';
 
+const lpad2 = number => ('0'+number).slice(-2)
+
 const SelectedEvents = ({ events, date }) => {
+
 	return (
 	<>
-		<h3 className="eventDateDisplay">{date.getFullYear()}-{('0' + (date.getMonth() + 1)).slice(-2)}-{('0' + date.getDate()).slice(-2)}</h3>
+		<h3 className="eventDateDisplay">{date.getFullYear()}-{lpad2(date.getMonth()+1)}-{lpad2(date.getDate())}</h3>
 		<div className="eventsGridDisplay">
 			{events?.map(e => <EventDisplay {...e} />)}
 		</div>
