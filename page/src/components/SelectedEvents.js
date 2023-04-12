4c96207ba60eed92507d51bfea3ef4e1297190b5
Damien Vander Stricht <damien.vanderstricht@ext.adeo.com>
2023-04-12 19:14:49 +0200

    Revert "Update SelectedEvents.js"
    This reverts commit 184756cf019904a3e1d859d4d5a63f8bd9febc5e.

--- page/src/components/SelectedEvents.js
+++ page/src/components/SelectedEvents.js
@@ -9,7 +9,7 @@ const formatDate = date => date.getFullYear()+"-"+lpad2(date.getMonth()+1)+"-"+l
 const SelectedEvents = ({ events, date }) => {
 	return (
 	<>
-		<h3 className="eventDateDisplay">{formatDate(date)}</h3>
+		<h3 className="eventDateDisplay">{(date.getFullYear()}-({('0' + date.getMonth() + 1).slice(-2)}-{('0' + date.getDate()).slice(-2)}</h3>
 		<div className="eventsGridDisplay">
 			{events?.map(e => <EventDisplay {...e} />)}
 		</div>
