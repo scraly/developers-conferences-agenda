0fdd17d9d2709a3786751ba3521bba144ff0ef6f
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-04-24 13:48:33 +0200

    fix: :bug: cause a conflict

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # This script can be runed locally in order to set up and run the website for development purpose
 
 cd page
-npm install -D
+npm install -D --force
 
 cd ../tools
 node mdValidator.js
