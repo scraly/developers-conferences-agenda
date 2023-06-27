6d4afcfa66213e75007aedab5cc8ed4ab4bf219e
Hanz <hanz@godot.id>
2023-06-27 09:02:56 +0700

    chore(workflow/ghpages): Remove the need for `--force` flag in `npm install`

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # This script can be runed locally in order to set up and run the website for development purpose
 
 cd page
-npm install -D --force
+npm install -D
 
 cd ../tools
 node mdValidator.js
