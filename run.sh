f4c35869842acac27cf2d109f51439db8058fd00
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2023-09-26 21:56:49 +0200

    run.sh: add RSS generation
    Signed-off-by: Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# This script can be runed locally in order to set up and run the website for development purpose
+# This script can be ran locally in order to set up and run the website for development purpose
 
 cd page
 npm install -D
 
 cd ../tools
+npm install -D
 node mdValidator.js
 node mdParser.js
 node generateIcs.js
+node generateRSS.js
 
 cd ../page
 npm start
