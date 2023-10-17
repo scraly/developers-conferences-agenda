c103ad0555472aad3bbafd20c8d33ea39cc6f63c
Jesús Espino <jespinog@gmail.com>
2023-10-17 09:29:05 +0200

    Adding title to the Filters header on open and on close

--- page/src/components/Filters/Filters.jsx
+++ page/src/components/Filters/Filters.jsx
@@ -7,7 +7,10 @@ import 'styles/Filters.css';
 const Filters = ({ query, callForPapers, onChange, onClose }) => {
   const [open, setOpen] = useState(false);
   return (
-    <div className={"filters " + (open ? 'open' : 'closed')}>
+    <div
+      className={"filters " + (open ? 'open' : 'closed')}
+      title={open ? 'Close filters' : 'Open filters'}
+    >
       <div
           className='filters-header'
           onClick={() => {
