ba0466804323360d1fba1f5b15ee669656895328
Jesús Espino <jespinog@gmail.com>
2023-10-17 16:06:04 +0200

    Adding a new List View to list events. (#668)
    * Adding list view
    
    * Small style improvements
    
    * Positioning correctly the views menu
    
    * Not showing the selected month in the list view when this is selected
    
    * Changing how the list is shown
    
    * Changing the style of events list
    
    * Making list view more compact

--- page/src/components/EventDisplay/EventDisplay.jsx
+++ page/src/components/EventDisplay/EventDisplay.jsx
@@ -1,16 +1,17 @@
 import 'styles/EventDisplay.css';
 import {formatEventDates} from './EventDisplay.utils';
 
-const EventDisplay = ({name, hyperlink, location, misc, date}) => {
+const EventDisplay = ({name, hyperlink, location, misc, date, dateOnTop=false}) => {
   return (
     <div className="eventCell">
+      {dateOnTop && <span className="when">{formatEventDates(date)}</span>}
       <div className="content">
         <b>{name}</b>
         {hyperlink ? <a href={hyperlink}>{new URL(hyperlink).hostname}</a> : ''}
         <span>{location}</span>
         <p dangerouslySetInnerHTML={{__html: misc}}></p>
       </div>
-      <span className="when">{formatEventDates(date)}</span>
+      {!dateOnTop && <span className="when">{formatEventDates(date)}</span>}
     </div>
   );
 };
---
+++ page/src/components/ListView/ListView.jsx
@@ -0,0 +1,94 @@
+import 'styles/ListView.css';
+import {ArrowRight} from 'lucide-react';
+
+import {filterEvents} from '../../utils';
+import {useCustomContext} from 'app.context';
+
+import EventDisplay from '../EventDisplay/EventDisplay';
+
+import {getYearEvents} from 'utils';
+
+const ListView = ({year}) => {
+  let events = getYearEvents(year);
+  const {userState} = useCustomContext();
+  events = filterEvents(events, userState.filters.callForPapers, userState.filters.query);
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const eventsByMonth = events.reduce((acc, cur) => {
+    const currentMonth = monthNames[new Date(cur.date[0]).getMonth()];
+    if (!acc[currentMonth]) {
+      acc[currentMonth] = [];
+    }
+    acc[currentMonth].push(cur);
+    if (cur.date.length > 1) {
+      const nextMonth = monthNames[new Date(cur.date[1]).getMonth()];
+      if (currentMonth !== nextMonth) {
+        if (!acc[nextMonth]) {
+          acc[nextMonth] = [];
+        }
+        acc[nextMonth].push(cur);
+      }
+    }
+    return acc;
+  }, {});
+
+  const formatDate = dates => {
+    const startDate = `${new Date(dates[0]).getDate()}-${monthNames[
+      new Date(dates[0]).getMonth()
+    ].slice(0, 3)}`;
+    let endDate = '';
+    if (dates.length > 1) {
+      endDate = new Date(dates[1]).getDate();
+      endDate = `${new Date(dates[1]).getDate()}-${monthNames[new Date(dates[1]).getMonth()].slice(
+        0,
+        3
+      )}`;
+    }
+    if (endDate) {
+      return (
+        <span>
+          {startDate}
+          <ArrowRight />
+          {endDate}
+        </span>
+      );
+    }
+    return <span>{startDate}</span>;
+  };
+
+  return (
+    <div className="listView">
+      {monthNames
+        .filter(m => eventsByMonth[m])
+        .map(month => (
+          <>
+            <h1>{month}</h1>
+            {eventsByMonth[month].map((e, i) => (
+              <div key={`${month}_ev_${i}`} className='event-list-entry'>
+                {formatDate(e.date)}
+                <b>{e.name}</b>
+                {e.hyperlink ? <a href={e.hyperlink}>{new URL(e.hyperlink).hostname}</a> : ''}
+                <span>{e.location}</span>
+                <span dangerouslySetInnerHTML={{__html: e.misc}}></span>
+              </div>
+            ))}
+          </>
+        ))}
+    </div>
+  );
+};
+
+export default ListView;
