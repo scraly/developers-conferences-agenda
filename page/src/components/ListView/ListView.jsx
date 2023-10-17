e18de6be3278747dee155d92678be2f41495f2c7
Jesús Espino <jespinog@gmail.com>
2023-10-17 10:41:48 +0200

    Small style improvements

--- page/src/components/ListView/ListView.jsx
+++ page/src/components/ListView/ListView.jsx
@@ -1,11 +1,15 @@
 import 'styles/ListView.css';
 
 import EventDisplay from '../EventDisplay/EventDisplay';
+import {filterEvents} from '../../utils';
+import {useCustomContext} from 'app.context';
 
 import { getYearEvents } from 'utils';
 
 const ListView = ({year}) => {
     let events = getYearEvents(year)
+    const {userState} = useCustomContext()
+    events = filterEvents(events, userState.filters.callForPapers, userState.filters.query)
 
     return <div className="listView">
         <div className="eventsGridDisplay">
