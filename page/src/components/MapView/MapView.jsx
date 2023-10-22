3e100df03de471d3181f6d02a6e094f155d1baeb
Jesús Espino <jespinog@gmail.com>
2023-10-22 17:00:56 +0200

    The markers icons were wrongly possitioned because it was lacking some position adjustments (#700)

--- page/src/components/MapView/MapView.jsx
+++ page/src/components/MapView/MapView.jsx
@@ -4,13 +4,18 @@ import geolocations from 'misc/geolocations.json';
 
 import iconMarker from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-//import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
 
 import { Icon } from 'leaflet'
 const myIcon = new Icon({
  iconUrl: iconMarker,
  shadowUrl: iconShadow,
- //iconRetinaUrl: iconRetina
+ iconRetinaUrl: iconRetina,
+ iconSize:    [25, 41],
+ iconAnchor:  [12, 41],
+ popupAnchor: [1, -34],
+ tooltipAnchor: [16, -28],
+ shadowSize:  [41, 41]
 })
 
 import {useMemo} from 'react';
