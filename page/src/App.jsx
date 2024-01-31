bebeebe7ef6ef8ebad85f00abdc718132120e788
Jesús Espino <jespinog@gmail.com>
2024-01-31 08:57:37 +0100

    Fixing the router on published pages

--- page/src/App.jsx
+++ page/src/App.jsx
@@ -1,5 +1,5 @@
 import {useEffect} from 'react';
-import {BrowserRouter as Router, Routes, Route, useParams, useNavigate, useSearchParams, createSearchParams} from "react-router-dom";
+import {HashRouter as Router, Routes, Route, useParams, useNavigate, useSearchParams, createSearchParams} from "react-router-dom";
 
 import {CalendarDays, CalendarClock} from 'lucide-react';
 
