b8cc593649f5ace18e30ea2dd892f09dcc69d60b
Hanz <hanz@godot.id>
2024-02-09 07:54:07 +0700

    feat(EventListEntry): Added `onClick` handler

--- page/src/components/ListView/Entry.jsx
+++ page/src/components/ListView/Entry.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ShortDate from "components/ShortDate/ShortDate";
 
 export function EventListEntry({ event }) {
+    let [showDetail, setShowDetail] = useState('hide');
     let hyperlink = <a></a>;
     let closedCaptionElement = null;
+
+    function helper_toggle_show() {
+        if (showDetail == 'hide') return 'show';
+        return 'hide';
+    }
     
     if (event.closedCaption) {
         closedCaptionElement = <img alt="Closed Captions" src="https://img.shields.io/static/v1?label=CC&message=Closed%20Captions&color=blue" />;
@@ -14,21 +20,23 @@ export function EventListEntry({ event }) {
     }
 
     return <div className="event-list-entry">
-        <header>
+        <header onClick={() => setShowDetail(helper_toggle_show)}>
             <ShortDate dates={event.date} />
             <div className="event-name-location">
                 <b>{event.name}</b>
                 <span>{event.location}</span>
             </div>
-            <div className="event-banners">
+            <div className={"event-banners " + helper_toggle_show()}>
                 <span dangerouslySetInnerHTML={{__html: event.misc}}></span>
                 <span>{closedCaptionElement}</span>
             </div>
             {hyperlink}
         </header>
-        <article>
+        <article className={showDetail}>
         </article>
-        <footer>
+        <footer className={showDetail}>
+            <span dangerouslySetInnerHTML={{__html: event.misc}}></span>
+            <span>{closedCaptionElement}</span>
         </footer>
     </div>;
 };
