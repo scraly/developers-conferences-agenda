ef32f6a662ce16c014de52e90a9c04ba278fb9fe
Hanz <40712686+HanzCEO@users.noreply.github.com>
2024-02-20 03:23:55 +0700

    Better Route Readability (Code Quality) (#864)
    * chore(page/index.html): Correction for code readibility
    
    * feat(geolocations): Added Udaipur, India (automated)
    
    * chore: Moved routes to their directory
    
    * fix(ViewSelector.css): Added better positioning
    
    * chore(Filters.css): Made filter button similar to others
    
    * fix(Filters.css): Removed `width: 10px`

---
+++ page/src/routes/mappage.jsx
@@ -0,0 +1,29 @@
+import Filters from "components/Filters/Filters";
+import MapView from "components/MapView/MapView";
+import YearSelector from "components/YearSelector/YearSelector";
+import { createSearchParams, useNavigate, useParams, useSearchParams } from "react-router-dom";
+
+export function MapPage() {
+    const {year} = useParams();
+    const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+
+    return (
+        <div className="dcaGrid">
+          <Filters/>
+          <div className="dcaContent">
+            <YearSelector
+              isMap={true}
+              year={parseInt(year, 10)}
+              onChange={year => {
+                navigate(`/${year}/map?${createSearchParams(searchParams)}`);
+              }}
+
+              view="map"
+            />
+
+            <MapView year={year} />
+        </div>
+      </div>
+    );
+}
