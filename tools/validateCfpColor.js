a4cf4dbfde067dbeebb2e311d12971ca8d3024da
Jesús Espino <jespinog@gmail.com>
2024-03-19 10:45:48 +0100

    Apply suggestions from code review
    Co-authored-by: Aurelie Vache <scraly@gmail.com>

--- tools/validateCfpColor.js
+++ tools/validateCfpColor.js
@@ -4,9 +4,9 @@ const allEvents = JSON.parse(fs.readFileSync('../page/src/misc/all-events.json')
 
 for (const event of allEvents) {
     if (event.cfp && event.cfp.untilDate && new Date(event.cfp.untilDate) < new Date() && event.misc.indexOf("color=green") !== -1) {
-        console.log("Invalid cfp green color: " + new Date(event.date[0]).toISOString() + " " + event.name);
+               console.log("Invalid CFP green color: " + event.name + " - " + event.location);
     }
     if (event.cfp && event.cfp.untilDate && new Date(event.cfp.untilDate) >= new Date() && event.misc.indexOf("color=red") !== -1) {
-        console.log("Invalid cfp red color: " + new Date(event.date[0]).toISOString() + " " + event.name);
+                console.log("Invalid CFP red color: " + event.name + " - " + event.location);
     }
 }
