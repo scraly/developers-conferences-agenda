1fac2eba09d702e9d8a68ef1259584b42cc23b80
Hanz <hi@hanz.dev>
2024-06-23 14:10:34 +0700

    fix(Filter): Fixed panel closing abruptly

--- page/src/App.jsx
+++ page/src/App.jsx
@@ -1,17 +1,5 @@
-import {useEffect} from 'react';
 import {HashRouter as Router, Routes, Route, useParams, useNavigate, useSearchParams, createSearchParams} from "react-router-dom";
 
-import {CalendarDays, CalendarClock} from 'lucide-react';
-
-import CalendarGrid from 'components/CalendarGrid/CalendarGrid';
-import ListView from 'components/ListView/ListView';
-import ViewSelector from 'components/ViewSelector/ViewSelector';
-import MapView from 'components/MapView/MapView';
-import YearSelector from 'components/YearSelector/YearSelector';
-import Filters from 'components/Filters/Filters';
-
-import {useHasYearEvents} from 'app.hooks';
-import SelectedEvents from 'components/SelectedEvents/SelectedEvents';
 import 'misc/fonts/inter/inter.css';
 import 'styles/App.css';
 
@@ -20,20 +8,29 @@ import { Year } from 'routes/year';
 import { DatePage } from 'routes/datepage';
 import { MapPage } from 'routes/mappage';
 import { ListPage } from 'routes/listpage';
+import { FilterContext } from 'contexts/FilterContext';
 
 const App = () => {
-  return (
-    <Router>
-      <h1 className="dcaTitle">Developer Conferences Agenda</h1>
-      <Routes path="/">
-        <Route path="" Component={Index} />
-        <Route path=":year" Component={Year} />
+  // TODO: DRY
+  const filtercontextdefaults = {
+    searchParams: {},
+    open: false
+  };
 
-        <Route path=":year/calendar/:month?/:date?" Component={DatePage} />
-        <Route path="/:year/map" Component={MapPage} />
-        <Route path="/:year/list" Component={ListPage} />
-      </Routes>
-    </Router>
+  return (
+    <FilterContext.Provider value={filtercontextdefaults}>
+      <Router>
+        <h1 className="dcaTitle">Developer Conferences Agenda</h1>
+        <Routes path="/">
+          <Route path="" Component={Index} />
+          <Route path=":year" Component={Year} />
+
+          <Route path=":year/calendar/:month?/:date?" Component={DatePage} />
+          <Route path="/:year/map" Component={MapPage} />
+          <Route path="/:year/list" Component={ListPage} />
+        </Routes>
+      </Router>
+    </FilterContext.Provider>
   );
 };
 
