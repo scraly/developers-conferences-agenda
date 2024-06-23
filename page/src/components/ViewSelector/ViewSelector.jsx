0b2fc92fb6c7e667d4638f9bf22e18f8674adde4
Hanz <hi@hanz.dev>
2024-06-23 14:50:44 +0700

    feat(Filters): Adjusted UI

--- page/src/components/ViewSelector/ViewSelector.jsx
+++ page/src/components/ViewSelector/ViewSelector.jsx
@@ -13,14 +13,17 @@ const ViewSelector = ({selected}) => {
       <Calendar
         className={selected === 'calendar' ? 'view-selector calendar-view selected' :  'view-selector calendar-view'}
         onClick={() => navigate(`/${year}/calendar?${createSearchParams(searchParams)}`)}
+        size={'32px'}
       />
       <List
         className={selected === 'list' ? 'view-selector list-view selected' : 'view-selector list-view'}
         onClick={() => navigate(`/${year}/list?${createSearchParams(searchParams)}`)}
+        size={'32px'}
       />
       <Map
         className={selected === 'map' ? 'view-selector map-view selected' : 'view-selector map-view'}
         onClick={() => navigate(`/${year}/map?${createSearchParams(searchParams)}`)}
+        size={'32px'}
       />
     </div>
   );
