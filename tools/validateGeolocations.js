79732b7a23a680f869418e869b1e9ec007398014
Jesús Espino <jespinog@gmail.com>
2024-08-28 18:04:24 +0200

    Increas the range for city duplication detection (#1122)
    Co-authored-by: Aurelie Vache <scraly@gmail.com>

--- tools/validateGeolocations.js
+++ tools/validateGeolocations.js
@@ -7,7 +7,7 @@ Object.keys(locations).forEach((city) => {
     if (city === city2) {
       return
     }
-    if (Math.abs(locations[city].longitude - locations[city2].longitude) < 0.001 && Math.abs(locations[city].latitude - locations[city2].latitude) < 0.001) {
+    if (Math.abs(locations[city].longitude - locations[city2].longitude) < 0.015 && Math.abs(locations[city].latitude - locations[city2].latitude) < 0.015) {
       console.error(`Duplicate coordinates: ${city} and ${city2}`)
       hasErrors = true
     }
