23c25b2da1e6b5c771f62aeeb3a2080d335f43cb
Aurelie Vache <scraly@gmail.com>
2024-09-11 19:39:32 +0000

    fix: cfp

--- tools/fixCFPColor.js
+++ tools/fixCFPColor.js
@@ -6,6 +6,7 @@ let readmeFile = fs.readFileSync('../README.md', 'utf-8')
 
 for (const event of allEvents) {
     if (event.cfp && event.cfp.untilDate && new Date(event.cfp.untilDate + (24 * 60 *60 * 1000)) < new Date() && event.misc.indexOf("color=green") !== -1) {
+        //console.log(event.cfp, event.misc)
         readmeFile = readmeFile.replace(event.cfp.until+"&color=green", event.cfp.until+"&color=red");
     }
 }
