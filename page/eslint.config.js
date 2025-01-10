aed29f1299219bfbaa11ff7059520c6e40d0ca85
Aurelie Vache <scraly@gmail.com>
2025-01-10 21:42:53 +0000

    feat: add linter and format

---
+++ page/eslint.config.js
@@ -0,0 +1,103 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+  "settings": {
+    "react": {
+      "createClass": "createReactClass", // Regex for Component Factory to use,
+                                         // default to "createReactClass"
+      "pragma": "React",  // Pragma to use, default to "React"
+      "fragment": "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
+      "version": "detect", // React version. "detect" automatically picks the version you have installed.
+                           // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+                           // Defaults to the "defaultVersion" setting and warns if missing, and to "detect" in the future
+      "defaultVersion": "", // Default React version to use when the version you have installed cannot be detected.
+                            // If not provided, defaults to the latest React version.
+      "flowVersion": "0.53" // Flow version
+    },
+    "propWrapperFunctions": [
+        // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+        "forbidExtraProps",
+        {"property": "freeze", "object": "Object"},
+        {"property": "myFavoriteWrapper"},
+        // for rules that check exact prop wrappers
+        {"property": "forbidExtraProps", "exact": true}
+    ],
+    "componentWrapperFunctions": [
+        // The name of any function used to wrap components, e.g. Mobx `observer` function. If this isn't set, components wrapped by these functions will be skipped.
+        "observer", // `property`
+        {"property": "styled"}, // `object` is optional
+        {"property": "observer", "object": "Mobx"},
+        {"property": "observer", "object": "<pragma>"} // sets `object` to whatever value `settings.react.pragma` is set to
+    ],
+    "formComponents": [
+      // Components used as alternatives to <form> for forms, eg. <Form endpoint={ url } />
+      "CustomForm",
+      {"name": "SimpleForm", "formAttribute": "endpoint"},
+      {"name": "Form", "formAttribute": ["registerEndpoint", "loginEndpoint"]}, // allows specifying multiple properties if necessary
+    ],
+    "linkComponents": [
+      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+      "Hyperlink",
+      {"name": "MyLink", "linkAttribute": "to"},
+      {"name": "Link", "linkAttribute": ["to", "href"]}, // allows specifying multiple properties if necessary
+    ],
+    "jsx-a11y": {
+      "polymorphicPropName": "as",
+      "attributes": {
+        "for": ["htmlFor", "for"]
+      }
+    }
+  }
+},
+{
+  rules: {
+      "react/prefer-stateless-function": "error",
+      "react/button-has-type": "error",
+      "react/no-unused-prop-types": "error",
+      "react/jsx-pascal-case": "error",
+      "react/jsx-no-script-url": "error",
+      "react/no-children-prop": "error",
+      "react/no-danger": "error",
+      "react/no-danger-with-children": "error",
+      "react/no-unstable-nested-components": ["error", { allowAsProps: true }],
+      "react/jsx-fragments": "error",
+      "react/destructuring-assignment": [
+        "error",
+        "always",
+        { destructureInSignature: "always" },
+      ],
+      "react/jsx-no-leaked-render": ["error", { validStrategies: ["ternary"] }],
+      "react/jsx-max-depth": ["error", { max: 5 }],
+      "react/function-component-definition": [
+        "warn",
+        { namedComponents: "arrow-function" },
+      ],
+      "react/jsx-key": [
+        "error",
+        {
+          checkFragmentShorthand: true,
+          checkKeyMustBeforeSpread: true,
+          warnOnDuplicates: true,
+        },
+      ],
+      "react/jsx-no-useless-fragment": "warn",
+      "react/jsx-curly-brace-presence": "warn",
+      "react/no-typos": "warn",
+      "react/display-name": "warn",
+      "react/self-closing-comp": "warn",
+      "react/jsx-sort-props": "warn",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-one-expression-per-line": "off",
+      "react/prop-types": "off",
+  }
+}
+];
