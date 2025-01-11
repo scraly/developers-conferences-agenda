e58714ccfbc0c96288e369fd08ccc9c67068cd56
Aurelie Vache <scraly@gmail.com>
2025-01-11 20:55:14 +0000

    feat: display in the RSS feed only events >= current year -1

--- tools/generateRSS.js
+++ tools/generateRSS.js
@@ -12,6 +12,10 @@ const f = new feed.Feed({
   description: "A list of tech conferences's date and CFP in order to help conferences organizers, speakers & attendees",
   id: "https://developers.events/",
   link: "https://developers.events/",
+  author: {
+    name: 'Aurélie Vache',
+    link: 'https://developers.events'
+  },
   feedLinks: {
     json: "https://developers.events/feed-events.json",
     atom: "https://developers.events/feed-events.xml"
@@ -20,22 +24,28 @@ const f = new feed.Feed({
 
 events = JSON.parse(fs.readFileSync('../page/src/misc/all-events.json'), 'utf-8');
 let now = new Date(today)
+
 events.forEach((event, idx) => {
-  now = new Date(now.getTime() + 1000)
-  f.addItem({
-    title: event.name,
-    author: [
-        {
-            name: 'developers.events',
-            link: 'https://github.com/scraly/developers-conferences-agenda'
-        }
-    ],
-    id: encodeURI(decodeURI(event.hyperlink)) + '/' + idx,
-    link: encodeURI(decodeURI(event.hyperlink)),
-    description: event.name,
-    content: event.name + " @ " + event.location + " - " + new Date(event.date[0]),
-    date: now,
-  })
+  // If event date[0] >= current year -1
+  let eventYear = new Date(event.date[0]).getFullYear()
+
+  if (eventYear >= now.getFullYear()-1) {
+    now = new Date(now.getTime() + 1000)
+    f.addItem({
+      title: event.name,
+      author: [
+          {
+              name: 'developers.events',
+              link: 'https://github.com/scraly/developers-conferences-agenda'
+          }
+      ],
+      id: encodeURI(decodeURI(event.hyperlink)) + '/' + idx,
+      link: encodeURI(decodeURI(event.hyperlink)),
+      description: event.name,
+      content: event.name + " @ " + event.location + " - " + new Date(event.date[0]),
+      date: now,
+    })
+  }
 })
 
 fs.writeFileSync(ATOM_OUTPUT, f.atom1());
