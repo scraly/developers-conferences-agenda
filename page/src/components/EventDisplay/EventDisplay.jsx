bacae5db31211d37613411f7097f1c14c684139b
Aurelie Vache <scraly@gmail.com>
2025-07-19 19:35:49 +0000

    feat: improve eventView a litte bit in calendarView

--- page/src/components/EventDisplay/EventDisplay.jsx
+++ page/src/components/EventDisplay/EventDisplay.jsx
@@ -2,9 +2,11 @@ import 'styles/EventDisplay.css';
 import {formatEventDates} from './EventDisplay.utils';
 import FavoriteButton from '../FavoriteButton/FavoriteButton';
 import { useFavoritesContext } from '../../contexts/FavoritesContext';
+import { flag } from 'country-emoji';
+import ShortDate from 'components/ShortDate/ShortDate';
 
-const EventDisplay = ({name, hyperlink, location, misc, closedCaptions, date, dateOnTop=false}) => {
-  const event = {name, hyperlink, location, misc, closedCaptions, date};
+const EventDisplay = ({name, hyperlink, location, misc, closedCaptions, date, dateOnTop=false, country}) => {
+  const event = {name, hyperlink, location, misc, closedCaptions, date, country};
   const eventId = `${name}-${date[0]}`;
   const { isFavorite } = useFavoritesContext();
   const isFav = isFavorite(eventId);
@@ -13,16 +15,19 @@ const EventDisplay = ({name, hyperlink, location, misc, closedCaptions, date, da
     <div className={`eventCell ${isFav ? 'favorite-event' : ''}`}>
       {dateOnTop ? <span className="when">{formatEventDates(date)}</span> : null}
       <div className="content">
+        <span className="when"><ShortDate dates={date} /></span>
+
         <div className="event-header">
-          <b>{name}</b>
+          <b>{hyperlink ? <a className="title" href={hyperlink} rel="noreferrer" target="_blank">{name}</a> : ''}</b>
           <FavoriteButton event={event} />
         </div>
-        {hyperlink ? <a href={hyperlink}>{new URL(hyperlink).hostname}</a> : ''}
-        <span>{location}</span>
-        <p dangerouslySetInnerHTML={{__html: misc}} />
+        <div className="country">
+            <span className="countryFlag">{country != "Online" ? flag(country) : '🌎'}</span>
+            <span className="countryName">{location}</span>
+        </div>
+        <p className="cfp" dangerouslySetInnerHTML={{__html: misc}} />
         {closedCaptions ? <span><img alt="Closed Captions" src="https://img.shields.io/static/v1?label=CC&message=Closed%20Captions&color=blue" /></span> : null}
       </div>
-      {!dateOnTop ? <span className="when">{formatEventDates(date)}</span> : null}
     </div>
   );
 };
