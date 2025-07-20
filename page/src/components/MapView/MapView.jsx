b8a7acb71b28b4a563b0364a1b45d11e5e8b39be
Aurelie Vache <scraly@gmail.com>
2025-07-20 15:53:52 +0000

    fix

--- page/src/components/MapView/MapView.jsx
+++ page/src/components/MapView/MapView.jsx
@@ -104,7 +104,7 @@ const MapView = () => {
               <div className={`event-map-entry ${isFav ? 'favorite-event' : ''}`} key={`ev_${i}`}>
                 <ShortDate dates={e.date} />
                 {e.hyperlink ? <a href={e.hyperlink} rel="noreferrer" target='_blank'>{e.name}</a> : <b>{e.name}</b>}
-                <span dangerouslySetInnerHTML={{__html: e.misc}} />
+                <span className="event-map-misc" dangerouslySetInnerHTML={{__html: e.misc}} />
                 {e.closedCaptions ? <span><img alt="Closed Captions" src="https://img.shields.io/static/v1?label=CC&message=Closed%20Captions&color=blue" /></span> : null}
                 <FavoriteButton event={e} />
               </div>
