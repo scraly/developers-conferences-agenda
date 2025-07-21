fd299b8e626b19727a29346c03b3f1b91f1bfa7c
Jesús Espino <jesus@gitpod.io>
2025-07-21 11:18:10 +0200

    Adding scrolling to the filters

--- page/src/components/Filters/Filters.jsx
+++ page/src/components/Filters/Filters.jsx
@@ -79,88 +79,90 @@ const Filters = ({ view }) => {
         <span className='filters-title'>Filters</span>
       </div>
 
-      <div className='filtersItem'>
-        <label htmlFor='filter-query'>Query:</label>
-        <input id='filter-query' onChange={(e) => onChange('query', e.target.value)} placeholder="Search..." type='text' value={search.query} />
-      </div>
-
-{tagsVisible ? <div className='filtersItem tags-filter'>
-          <label>Tags:</label>
-          <TagMultiSelect
-            onChange={handleTagsChange}
-            selectedTags={selectedTags}
-          />
-          <SelectedTags
-            onRemoveTag={handleRemoveTag}
-            selectedTags={selectedTags}
-          />
-        </div> : null}
-
-      <div className='filtersItem'>
-        <label htmlFor='filter-until'>CFP Until:</label>
-        <input id="filter-until" onChange={(e) => onChange('untilDate', e.target.value)} type="date" value={search.untilDate} />
-      </div>
-      
-      <div className='filtersList'>
-
-        {view != "cfp" ? 
+      <div className='filters-content'>
         <div className='filtersItem'>
-          <input checked={search.callForPapers == 'true'} id='filter-call-for-papers' onChange={(e) => onChange('callForPapers', e.target.checked)} type='checkbox' />
-          <label htmlFor='filter-call-for-papers'>Call For Papers Open</label>
-        </div> : ''}
+          <label htmlFor='filter-query'>Query:</label>
+          <input id='filter-query' onChange={(e) => onChange('query', e.target.value)} placeholder="Search..." type='text' value={search.query} />
+        </div>
 
-        {view != "cfp" ?
-        <div className='filtersItem'>
-          <input checked={search.closedCaptions == 'true'} id='filter-closed-captions' onChange={(e) => onChange('closedCaptions', e.target.checked)} type='checkbox' />
-          <label htmlFor='filter-closed-captions'>Closed Captions</label>
-        </div> : ''}
-
-        {view != "cfp" ?
-        <div className='filtersItem'>
-          <input checked={search.scholarship == 'true'} id='filter-scholarship' onChange={(e) => onChange('scholarship', e.target.checked)} type='checkbox' />
-          <label htmlFor='filter-scholarship'>Scholarship</label>
-        </div> : ''}
+  {tagsVisible ? <div className='filtersItem tags-filter'>
+            <label>Tags:</label>
+            <TagMultiSelect
+              onChange={handleTagsChange}
+              selectedTags={selectedTags}
+            />
+            <SelectedTags
+              onRemoveTag={handleRemoveTag}
+              selectedTags={selectedTags}
+            />
+          </div> : null}
 
         <div className='filtersItem'>
-          <input checked={search.online == 'true'} id='filter-online' onChange={(e) => onChange('online', e.target.checked)} type='checkbox' />
-          <label htmlFor='filter-online'>Online</label>
+          <label htmlFor='filter-until'>CFP Until:</label>
+          <input id="filter-until" onChange={(e) => onChange('untilDate', e.target.value)} type="date" value={search.untilDate} />
+        </div>
+        
+        <div className='filtersList'>
+
+          {view != "cfp" ? 
+          <div className='filtersItem'>
+            <input checked={search.callForPapers == 'true'} id='filter-call-for-papers' onChange={(e) => onChange('callForPapers', e.target.checked)} type='checkbox' />
+            <label htmlFor='filter-call-for-papers'>Call For Papers Open</label>
+          </div> : ''}
+
+          {view != "cfp" ?
+          <div className='filtersItem'>
+            <input checked={search.closedCaptions == 'true'} id='filter-closed-captions' onChange={(e) => onChange('closedCaptions', e.target.checked)} type='checkbox' />
+            <label htmlFor='filter-closed-captions'>Closed Captions</label>
+          </div> : ''}
+
+          {view != "cfp" ?
+          <div className='filtersItem'>
+            <input checked={search.scholarship == 'true'} id='filter-scholarship' onChange={(e) => onChange('scholarship', e.target.checked)} type='checkbox' />
+            <label htmlFor='filter-scholarship'>Scholarship</label>
+          </div> : ''}
+
+          <div className='filtersItem'>
+            <input checked={search.online == 'true'} id='filter-online' onChange={(e) => onChange('online', e.target.checked)} type='checkbox' />
+            <label htmlFor='filter-online'>Online</label>
+          </div>
+
+          <div className='filtersItem'>
+            <input checked={search.favorites == 'true'} id='filter-favorites' onChange={(e) => onChange('favorites', e.target.checked)} type='checkbox' />
+            <label htmlFor='filter-favorites'>Favorites</label>
+          </div>
         </div>
 
         <div className='filtersItem'>
-          <input checked={search.favorites == 'true'} id='filter-favorites' onChange={(e) => onChange('favorites', e.target.checked)} type='checkbox' />
-          <label htmlFor='filter-favorites'>Favorites</label>
-        </div>
-      </div>
-
-      <div className='filtersItem'>
-        <label htmlFor='filter-region'>Region:</label>
-        <select id='filter-region' onChange={(e) => onChange('region', e.target.value)} value={search.region}>
-          <option value=''>All</option>
-          {regions.map((c) => (<option key={c} value={c}>{c}</option>))}
-        </select>
-      </div>
-
-      {countriesList ? <div className='filtersItem'>
-          <label htmlFor='filter-country'>Country:</label>
-          <select id='filter-country' onChange={(e) => onChange('country', e.target.value)} value={search.country}>
+          <label htmlFor='filter-region'>Region:</label>
+          <select id='filter-region' onChange={(e) => onChange('region', e.target.value)} value={search.region}>
             <option value=''>All</option>
-            {countriesList.map((c) => (<option key={c} value={c}>{c}</option>))}
+            {regions.map((c) => (<option key={c} value={c}>{c}</option>))}
           </select>
-        </div> : null}
+        </div>
 
-       {view == "list" ?
-       <div className='filters-header'>
-        <span className='filters-title'>Sorting</span>
-      </div> : ''}
-      
-      {view == "list" ?
-      <div className='filtersItem'>
-        <label htmlFor='filter-sort'>Sort:</label>
-        <select id='filter-sort' onChange={(e) => onChange('sort', e.target.value)} value={search.sort}>
-          <option value='date'>Event Start Date</option>
-          <option value='cfp'>CFP Close Date</option>
-        </select>
-      </div> : ''}
+        {countriesList ? <div className='filtersItem'>
+            <label htmlFor='filter-country'>Country:</label>
+            <select id='filter-country' onChange={(e) => onChange('country', e.target.value)} value={search.country}>
+              <option value=''>All</option>
+              {countriesList.map((c) => (<option key={c} value={c}>{c}</option>))}
+            </select>
+          </div> : null}
+
+         {view == "list" ?
+         <div className='filters-header'>
+          <span className='filters-title'>Sorting</span>
+        </div> : ''}
+        
+        {view == "list" ?
+        <div className='filtersItem'>
+          <label htmlFor='filter-sort'>Sort:</label>
+          <select id='filter-sort' onChange={(e) => onChange('sort', e.target.value)} value={search.sort}>
+            <option value='date'>Event Start Date</option>
+            <option value='cfp'>CFP Close Date</option>
+          </select>
+        </div> : ''}
+      </div>
 
     </div>
   );
