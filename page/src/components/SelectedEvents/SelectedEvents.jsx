72661aeb3597405954196255c3db26adc5b99d43
scraly <scraly@gmail.com>
2026-05-10 15:54:05 +0200

    fix: calendarview - click on a day

--- page/src/components/SelectedEvents/SelectedEvents.jsx
+++ page/src/components/SelectedEvents/SelectedEvents.jsx
@@ -19,6 +19,7 @@ const SelectedEvents = () => {
   const currentDate = Number.isNaN(parsedDate) ? undefined : new Date(parsedDate);
 
   const parsedMonth = parseInt(month, 10);
+  const isDayView = parsedMonth === -1 && !!currentDate;
   let currentMonth = Number.isNaN(parsedMonth) ? -1 : parsedMonth;
 
   if (currentMonth === -1 && currentDate) {
@@ -32,7 +33,7 @@ const SelectedEvents = () => {
   const yearEvents = useYearEvents()
   const monthEvents = useMonthEvents(yearEvents, resolvedMonth)
   const dayEvents = useDayEvents(monthEvents, currentDate)
-  const events = currentMonth != -1 ?  monthEvents : dayEvents;
+  const events = isDayView ? dayEvents : monthEvents;
 
   useEffect(() => {
     setTimeout(() => {
@@ -42,7 +43,7 @@ const SelectedEvents = () => {
 
   let previous = '',
     next = '';
-  if (currentMonth !== -1 && year) {
+  if (!isDayView && currentMonth !== -1 && year) {
     if (currentMonth > 0)
       previous = (
         <ArrowLeftCircle
@@ -59,7 +60,7 @@ const SelectedEvents = () => {
           }
         />
       );
-  } else if (currentDate) {
+  } else if (isDayView && currentDate) {
     const dateYear = currentDate.getFullYear();
     const firstDay = new Date(dateYear, 0, 1).getTime();
     const lastDay = new Date(dateYear, 12, 0).getTime();
@@ -93,7 +94,7 @@ const SelectedEvents = () => {
         <>
           <h3 className="eventDateDisplay" ref={scrollToRef}>
             {previous}
-            <span>{getTranslatedMonthName(currentMonth, t) || formatDate(currentDate)}</span>
+            <span>{isDayView ? formatDate(currentDate) : getTranslatedMonthName(currentMonth, t)}</span>
             {next}
             </h3>
             <EventCount events={events} />
