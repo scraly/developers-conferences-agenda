0a30eb1fd61a6882c5efaf87f2c15ab56541e9e0
scraly <scraly@gmail.com>
2026-05-11 21:58:19 +0200

    add: pined dependencies

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 # This script can be ran locally in order to set up and run the website for development purpose
 
 cd page
-npm install -D
+npm ci
 
 cd ../tools
-npm install -D
+npm ci
 node mdValidator.js
 node mdParser.js
 node validateRegions.js
