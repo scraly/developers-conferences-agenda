6272cda9976a44254fbae508a36da0e44ec4a5f1
scraly <scraly@gmail.com>
2026-05-29 18:10:11 +0200

    add: save and restore lastVisitedView from the localStorage

--- page/src/routes/year.jsx
+++ page/src/routes/year.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { getLastVisitedView } from 'misc/lastVisitedView';
 
 export function Year() {
     const {year} = useParams();
     const navigate = useNavigate();
     useEffect(() => {
-        return navigate('/' + year + '/calendar');
-    }, [year]);
+        return navigate(`/${year}/${getLastVisitedView()}`, { replace: true });
+    }, [year, navigate]);
 }
