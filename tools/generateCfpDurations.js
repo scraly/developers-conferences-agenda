169acf333ce7c4737ce478a9a813ddda0087a8dd
scraly <scraly@gmail.com>
2026-09-06 16:50:58 +0200

    fix: security

--- tools/generateCfpDurations.js
+++ tools/generateCfpDurations.js
@@ -54,13 +54,13 @@ const upsertEntries = entries => {
 };
 
 const htmlToText = html => html
-  .replace(/<script\b[^>]*>[\s\S]*?<\/script>/gi, ' ')
-  .replace(/<style\b[^>]*>[\s\S]*?<\/style>/gi, ' ')
+  .replace(/<script\b[^>]*>[\s\S]*?<\/script\s*>/gi, ' ')
+  .replace(/<style\b[^>]*>[\s\S]*?<\/style\s*>/gi, ' ')
   .replace(/<[^>]+>/g, ' ')
   .replace(/&nbsp;/gi, ' ')
-  .replace(/&amp;/gi, '&')
   .replace(/&#39;|&apos;/gi, "'")
   .replace(/&quot;/gi, '"')
+  .replace(/&amp;/gi, '&')
   .replace(/([\p{Ll}])(\p{Lu})/gu, '$1 $2')
   .replace(/\s+/g, ' ');
 
